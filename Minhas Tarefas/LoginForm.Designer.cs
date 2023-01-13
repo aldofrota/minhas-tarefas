034e75f1dbf071ea77b934ffa035fb03daa20ee2
Aldo Frota <aldofrotadev@gmail.com>
2023-01-12 22:20:50 -0300

    Subindo os arquivos

---
+++ Minhas Tarefas/LoginForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Minhas_Tarefas
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelEmail = new System.Windows.Forms.Label();
+            this.TextBoxEmail = new System.Windows.Forms.TextBox();
+            this.TextBoxPassword = new System.Windows.Forms.TextBox();
+            this.LabelPassword = new System.Windows.Forms.Label();
+            this.BtnEntrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LabelEmail
+            // 
+            this.LabelEmail.Font = new System.Drawing.Font("Bahnschrift Light SemiCondensed", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelEmail.Location = new System.Drawing.Point(49, 60);
+            this.LabelEmail.Margin = new System.Windows.Forms.Padding(0);
+            this.LabelEmail.Name = "LabelEmail";
+            this.LabelEmail.Size = new System.Drawing.Size(51, 23);
+            this.LabelEmail.TabIndex = 0;
+            this.LabelEmail.Text = "Email";
+            this.LabelEmail.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // TextBoxEmail
+            // 
+            this.TextBoxEmail.BackColor = System.Drawing.SystemColors.Window;
+            this.TextBoxEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBoxEmail.Cursor = System.Windows.Forms.Cursors.Default;
+            this.TextBoxEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBoxEmail.Location = new System.Drawing.Point(52, 86);
+            this.TextBoxEmail.Name = "TextBoxEmail";
+            this.TextBoxEmail.Size = new System.Drawing.Size(233, 19);
+            this.TextBoxEmail.TabIndex = 1;
+            // 
+            // TextBoxPassword
+            // 
+            this.TextBoxPassword.BackColor = System.Drawing.SystemColors.Window;
+            this.TextBoxPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBoxPassword.Cursor = System.Windows.Forms.Cursors.Default;
+            this.TextBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBoxPassword.Location = new System.Drawing.Point(52, 145);
+            this.TextBoxPassword.Name = "TextBoxPassword";
+            this.TextBoxPassword.PasswordChar = '*';
+            this.TextBoxPassword.ShortcutsEnabled = false;
+            this.TextBoxPassword.Size = new System.Drawing.Size(233, 19);
+            this.TextBoxPassword.TabIndex = 2;
+            this.TextBoxPassword.UseSystemPasswordChar = true;
+
+            // 
+            // LabelPassword
+            // 
+            this.LabelPassword.Font = new System.Drawing.Font("Bahnschrift Light SemiCondensed", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelPassword.Location = new System.Drawing.Point(49, 119);
+            this.LabelPassword.Name = "LabelPassword";
+            this.LabelPassword.Size = new System.Drawing.Size(70, 23);
+            this.LabelPassword.TabIndex = 3;
+            this.LabelPassword.Text = "Password";
+            this.LabelPassword.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // BtnEntrar
+            // 
+            this.BtnEntrar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BtnEntrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.BtnEntrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnEntrar.FlatAppearance.BorderSize = 0;
+            this.BtnEntrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnEntrar.ForeColor = System.Drawing.SystemColors.Control;
+            this.BtnEntrar.Location = new System.Drawing.Point(52, 193);
+            this.BtnEntrar.Name = "BtnEntrar";
+            this.BtnEntrar.Size = new System.Drawing.Size(233, 41);
+            this.BtnEntrar.TabIndex = 4;
+            this.BtnEntrar.Text = "Entrar";
+            this.BtnEntrar.UseVisualStyleBackColor = false;
+            // 
+            // LoginForm
+            // 
+            this.ClientSize = new System.Drawing.Size(334, 293);
+            this.Controls.Add(this.BtnEntrar);
+            this.Controls.Add(this.LabelPassword);
+            this.Controls.Add(this.TextBoxPassword);
+            this.Controls.Add(this.TextBoxEmail);
+            this.Controls.Add(this.LabelEmail);
+            this.Name = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelEmail;
+        private System.Windows.Forms.TextBox TextBoxEmail;
+        private System.Windows.Forms.TextBox TextBoxPassword;
+        private System.Windows.Forms.Label LabelPassword;
+        private System.Windows.Forms.Button BtnEntrar;
+    }
+}
+
---
+++ Minhas Tarefas/LoginForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Minhas_Tarefas
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            string caminho = "../../imgs/";
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.CenterToScreen();
+            this.Text = "Minhas Tarefas";
+            this.Icon = new Icon(caminho + "icon.ico");
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+        }
+    }
+}
